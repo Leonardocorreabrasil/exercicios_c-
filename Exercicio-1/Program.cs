d1f211d9439310616b032a7ed29d2f7e98ed2375
Leonardo Correa Brasil <correabrasilleonardo@gmail.com>
2021-12-02 10:18:36 -0300

    adicionando exercicios

---
+++ Exercicio-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// imprima seu nome
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leonardo correa brasil");
+        }
+    }
+}
---
+++ Exercicio-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// crie um algoritmo que imprima o produto entre 28 e 43
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var produto = 28 * 43;
+
+            Console.WriteLine($"o produto entre os dois é: {produto}");
+
+        }
+    }
+}
