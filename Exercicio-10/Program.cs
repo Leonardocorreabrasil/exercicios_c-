12110e57ff279ee0e8cb789a6f762bde772fec8f
Leonardo Correa Brasil <correabrasilleonardo@gmail.com>
2022-01-07 09:49:49 -0300

    adicionando exercicios

---
+++ Exercicio-10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+// ler um numero real e imprimir a terça parte deste nuemro
+namespace Exercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo invC = CultureInfo.InvariantCulture;
+            Console.WriteLine("entre com um numero com ponto: ");
+            double num = double.Parse(Console.ReadLine(), invC);
+            var result = num / 3.0;
+            Console.WriteLine(result.ToString("F1", invC));
+
+
+
+        }
+    }
+}
---
+++ Exercicio-9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// ler dois numeros inteiros e imprimnir o produto
+namespace Exercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entre com um numero: ");
+            var num1 = Console.ReadLine();
+            Console.WriteLine("entre com outro numero: ");
+            var num2 = Console.ReadLine();
+
+            var produto = Int32.Parse(num1) * Int32.Parse(num2);
+
+            Console.WriteLine($"produto: {produto}");
+
+        }
+    }
+}
