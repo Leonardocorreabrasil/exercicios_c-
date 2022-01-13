e2cc69bad3072d122a6ea26a75929ca7ce41fefe
Leonardo Correa Brasil <correabrasilleonardo@gmail.com>
2022-01-13 07:40:37 -0300

    adicionando exercicios

---
+++ Exercicio-14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+// entrar com um angulo em graus e imprimir: seno, cosseno, tangente, secante, cossecante e cotangente deste angulo
+
+namespace Exercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo invC = CultureInfo.InvariantCulture;
+            Console.WriteLine("digite um angulo em graus: "); ;
+            var angulo = double.Parse(Console.ReadLine(), invC);
+
+            var rang = angulo * Math.PI / 180.00;
+            Console.WriteLine($"seno: {Math.Sin(rang).ToString("F2", invC)}");
+            Console.WriteLine($"cosseno: {Math.Cos(rang).ToString("F2", invC)}");
+            Console.WriteLine($"tangente: {Math.Tan(rang).ToString("F2", invC)}");
+            Console.WriteLine($"cossecante: {Math.Cosh(rang).ToString("F2", invC)}");
+            Console.WriteLine($"secante: {Math.Sinh(rang).ToString("F2", invC)}");
+            Console.WriteLine($"cotangente: {Math.Cosh(rang).ToString("F2", invC)}");
+
+
+
+        }
+    }
+}
---
+++ Exercicio-15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+//entrar com um nuemro e imprimir o lagaritmo desse nuemro na base 10
+namespace Exercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo invC = CultureInfo.InvariantCulture;
+            Console.WriteLine("entre com o logaritmando: ");
+            var num = double.Parse(Console.ReadLine(), invC);
+
+            var logaritmo = Math.Log10(num).ToString("F1", invC);
+            Console.WriteLine($"logaritmo: {logaritmo}");
+
+        }
+    }
+}
