17868aca0b15f06acac310d6eb7553caf69bd0c8
Leonardo Correa Brasil <correabrasilleonardo@gmail.com>
2022-01-31 07:53:05 -0300

    adicionando exercicios

---
+++ Exercicio-21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*entrar com nome e idade e imrprimir a saida*/
+namespace Exercicio_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite nome: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine("digite a idade: ");
+            var idade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"nome: {nome}");
+            Console.WriteLine($"idade: {idade}");
+        }
+    }
+}
---
+++ Exercicio-22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+/*Em epocas de pouco dinheiro, os comerciantes estão procurando aumentar suas vendas oferecendo desconto. faça um algoritmo que possa entrar com o valor de um produto e imprima o novo valor tendo em vista que o desconto foi de 9%*/
+namespace Exercicio_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo invC = CultureInfo.InvariantCulture;
+            Console.WriteLine("digite o valor do produto: ");
+            var valorProduto = double.Parse(Console.ReadLine(), invC);
+
+            var novoPreco = valorProduto * 0.91;
+
+            Console.WriteLine($"preço com desconto {novoPreco.ToString("F2", invC)}");
+        }
+    }
+}
