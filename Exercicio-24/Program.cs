4337b54046a0877a38ed29b4f83528162e01e646
Leonardo Correa Brasil <correabrasilleonardo@gmail.com>
2022-02-16 00:27:05 -0300

    adicionando exercicios

---
+++ Exercicio-24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+/*Efetuar o calculo da quantidade de litros de combustiveis gastos em um viegem, sabendo-se qiue o carro faze=z 12 km coom um litro. deverao ser fornecidos o tempo gasto na viagem e a velocidade média */
+namespace Exercicio_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("qual o tempo de viagem? ");
+            var tempoViagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("qual a velocidade média de viagem? ");
+            var velocidadeMedia = int.Parse(Console.ReadLine());
+
+            var distancia = tempoViagem * velocidadeMedia;
+
+            var litrosUsados = distancia / 12;
+
+
+            Console.WriteLine($"velocidade média: {velocidadeMedia} Km/h");
+            Console.WriteLine($"tempo de viagem: {tempoViagem.ToString("0.00", CultureInfo.CreateSpecificCulture("en-US"))}");
+            Console.WriteLine($"distancia percorrida: {distancia.ToString("F0")} Km");
+            Console.WriteLine($"quantidade de litros utilizados: {litrosUsados.ToString("F1", CultureInfo.CreateSpecificCulture("en-US"))} litros ");
+        }
+
+
+    }
+}
---
+++ Exercicio-25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+/* efetuar o calculo do valor e uma prestação em atraso, utilizando a formula:
+prestação = valor + (valor*(taxa/100)* tempo)*/
+namespace Exercicio_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("digite o valor da prestaççao: ");
+            var valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("digite a taxa: ");
+            var taxa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("digite o tempo em meses: ");
+            var tempo = int.Parse(Console.ReadLine());
+
+            var prestacao = valor + (valor * (taxa / 100) * tempo);
+
+            Console.WriteLine($"o valor da prestação em atraso é: {prestacao.ToString("F2", CultureInfo.InvariantCulture)};");
+        }
+    }
+}
